6e51aa8cc44f0ef7091ed43fdc400eb796a1a1c8
imcoddy <imcoddy@gmail.com>
2015-07-20 20:24:35 +0900

    Add filter substring and make short version of address

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,13 +2,18 @@
 var bitcore = require('bitcore');
 
 angular.module('passworkApp')
-  .filter('brainAddress', function(){
-    return function(text){
+  .filter('brainAddress', function() {
+    return function(text) {
       var thing = text || '';
       var value = bitcore.deps.Buffer(thing);
       var hash = bitcore.crypto.Hash.sha256(value);
       var privateKey = new bitcore.PrivateKey(hash);
       return privateKey.toAddress().toString();
+    };
+  })
+  .filter('substring', function() {
+    return function(str, start, end) {
+      return str.substring(start, end);
     };
   })
   .controller('MainCtrl', function($scope, $http, socket) {
